e46ae3c84d2916f1cb87d6206eaefc72d6a92a87
Bohdan <bogdan.tkachivskiy@gmail.com>
2025-02-13 16:36:19 +0100

    Initial Commit

---
+++ Pet Translator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Pet Translator
+//
+//  Created by Богдан Ткачивський on 13/02/2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
